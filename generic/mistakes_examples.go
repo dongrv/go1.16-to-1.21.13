5e8d257c5c1c5205770fea95291c3d58076e753d
dongrvv <vitory.dong@gmail.com>
2025-05-16 10:30:39 +0800

    docs:约束类型为空的情况

--- generic/mistakes_examples.go
+++ generic/mistakes_examples.go
@@ -2,7 +2,7 @@ package generic
 
 // 编译器提示报错：
 
-// type NewType[T int | string] T // Cannot use a type parameter as RHS in type declaratio. 不允许新定义的泛型类型只有类型形参，无意义。
+// type NewType[T int | string] T // Cannot use a type parameter as RHS in type declaration. 不允许新定义的泛型类型只有类型形参，无意义。
 // type NewType1 [T * int][]T // 编译器会认为 T *int是一个表达式，在只有一个类型约束时，需要改成 [T *int,] 才能识别
 // type NewType2 [T *int|*float32,] // 但是约束存在多个指针类型时，约束尾部增加逗号也是不合法的，无法识别为有效的泛型约束
 
@@ -30,7 +30,15 @@ type IByte interface { // 交集，IByte代表string和[]byte的交集 ~[]byte
 	~[]byte
 }
 
-type EmptySet interface { // 空集，没有相交的类型，可以编译通过但没有实际意义
+type EmptySet interface { // 约束类型为空，没有相交的类型，可以编译通过但没有实际意义
 	~float64
 	int
+}
+
+type INumber3 interface {
+	EmptySet
+}
+
+func Hello[T INumber3](t T) {
+	println(t)
 }
--- generic/mistakes_examples_test.go
+++ generic/mistakes_examples_test.go
@@ -6,4 +6,8 @@ func TestNewTypeOK(t *testing.T) {
 	a, b := 1, 2
 	n := NewTypeOK[*int]{&a, &b}
 	t.Logf("%v", n)
+
+	t.Run("Hello", func(t *testing.T) {
+		//Hello(1) Cannot use int as the type INumber3 Type does not implement constraint 'INumber3' because constraint type set is empty
+	})
 }
