3f9c829d344bb87d4674b07345c4bff598cf6fc7
dongrvv <vitory.dong@gmail.com>
2025-05-22 19:04:49 +0800

    test:泛型类型的状态机和容器

--- generic/train.go
+++ generic/train.go
@@ -1,8 +1,15 @@
 package generic
 
 import (
+	"bytes"
+	"cmp"
+	"errors"
 	"fmt"
+	"math"
+	"slices"
 	"strings"
+	"sync"
+	"time"
 )
 
 // 双向链表
@@ -123,8 +130,8 @@ type ValidatorDecorator[T any] struct {
 
 func (v *ValidatorDecorator[T]) Decorate(f func(T) T) func(T) T {
 	return func(t T) T {
-		var zero T
 		if !v.validate(t) {
+			var zero T
 			fmt.Println("validate fail")
 			return zero
 		}
@@ -137,4 +144,249 @@ func ApplyDecorator[T any](f func(T) T, decorators ...Decorator[T]) func(T) T {
 		f = decorator.Decorate(f)
 	}
 	return f
+}
+
+func SortByKey[T any, K cmp.Ordered](ts []T, keyFunc func(T) K, ascending bool) {
+	slices.SortFunc(ts, func(a, b T) int {
+		keyA, keyB := keyFunc(a), keyFunc(b)
+		if keyA < keyB {
+			if ascending {
+				return -1
+			}
+			return 1
+		}
+		if keyA > keyB {
+			if ascending {
+				return 1
+			}
+			return -1
+		}
+		return 0
+	})
+}
+
+// 泛型数据库接口
+
+type Marshaller interface {
+	~struct{} | ~string
+	Marshal() ([]byte, error)
+}
+
+type DB[T Marshaller] interface {
+	Get(id string) (T, error)
+	Put(id string, data T) error
+}
+
+type MyString string
+
+func (m MyString) Marshal() ([]byte, error) {
+	return []byte(m), nil
+}
+
+type DBInstance[T Marshaller] struct {
+	store map[string]T
+}
+
+func (db *DBInstance[T]) Get(id string) (T, error) {
+	if v, ok := db.store[id]; ok {
+		return v, nil
+	}
+	var zero T
+	return zero, errors.New("no data")
+}
+
+func (db *DBInstance[T]) Put(id string, data T) error {
+	db.store[id] = data
+	return nil
+}
+
+// Pool
+
+type Pool[T ~struct{} | bytes.Buffer] struct {
+	pool sync.Pool
+}
+
+func (p *Pool[T]) Get() T {
+	v, ok := p.pool.Get().(T) // 需要显示的断言
+	if ok {
+		return v
+	}
+	var zero T
+	return zero
+}
+
+func (p *Pool[T]) Put(t T) {
+	p.pool.Put(t)
+}
+
+// 向量运算
+
+type IVectorNumber interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64 |
+		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | uintptr |
+		~float32 | ~float64
+}
+
+type Vector[T IVectorNumber] struct{}
+
+// Add 向量加法
+func (v *Vector[T]) Add(a, b []T) []T {
+	if len(a) != len(b) {
+		return nil
+	}
+	result := make([]T, len(a))
+	for i := range a {
+		result[i] = a[i] + b[i]
+	}
+	return result
+}
+
+// Dot 向量点积
+func (v *Vector[T]) Dot(a, b []T) T {
+	var zero T
+	if len(a) != len(b) {
+		return zero
+	}
+	var sum T
+	for i := range a {
+		sum += a[i] + b[i]
+	}
+	return sum
+}
+
+// Normalize 向量归一化
+func (v *Vector[T]) Normalize(vec []T) []T {
+	magnitude := v.Magnitude(vec)
+	result := make([]T, len(vec))
+	for i := range vec {
+		result[i] = vec[i] / magnitude
+	}
+	return result
+}
+
+// Magnitude 向量模长
+func (v *Vector[T]) Magnitude(vec []T) T {
+	var sum T
+	for _, v := range vec {
+		sum += v * v
+	}
+	return T(math.Sqrt(float64(sum)))
+}
+
+// 泛型状态机
+
+type State interface {
+	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64
+}
+
+type StateMachine[S State, E comparable] struct {
+	mu     sync.RWMutex
+	state  S             // 当前状态
+	bundle map[S]map[E]S // 起始状态->触发事件->结束状态
+}
+
+func NewStateMachine[S State, E comparable]() *StateMachine[S, E] {
+	return &StateMachine[S, E]{bundle: make(map[S]map[E]S)}
+}
+
+func (s *StateMachine[S, E]) AddTransition(from S, event E, to S) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if _, ok := s.bundle[from]; !ok {
+		s.bundle[from] = make(map[E]S)
+	}
+	if _, ok := s.bundle[from][event]; !ok {
+		s.bundle[from][event] = to
+	}
+}
+
+func (s *StateMachine[S, E]) Trigger(event E) (S, error) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	var zero S
+	if _, ok := s.bundle[s.state]; !ok {
+		return zero, errors.New("state error")
+	}
+	if _, ok := s.bundle[s.state][event]; !ok {
+		return zero, errors.New("not found event")
+	}
+	s.state = s.bundle[s.state][event]
+	return s.state, nil
+}
+
+// 泛型依赖容器
+
+type Container[T any] struct {
+	mu       sync.RWMutex
+	registry map[string]interface{}
+}
+
+func NewContainer[T any]() *Container[T] {
+	return &Container[T]{registry: map[string]interface{}{}}
+}
+
+func (c *Container[T]) RegisterInstance(name string, instance T) {
+	c.mu.Lock()
+	c.registry[name] = instance
+	c.mu.Unlock()
+}
+
+func (c *Container[T]) RegisterFactory(name string, fn func(container *Container[T]) T) {
+	c.mu.Lock()
+	c.registry[name] = fn
+	c.mu.Unlock()
+}
+
+func (c *Container[T]) Get(name string) (T, error) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
+	if val, ok := c.registry[name]; ok {
+		switch v := val.(type) {
+		case T:
+			return v, nil
+		case func(container *Container[T]) T:
+			return v(c), nil
+		default:
+			var zero T
+			return zero, fmt.Errorf("type mismatch for %s: expected: %T or func(*Container) %T", name, zero, zero)
+		}
+	}
+	var zero T
+	return zero, fmt.Errorf("no registration found %s", name)
+}
+
+type Logger interface {
+	Log(msg string)
+}
+
+type ConsoleLogger struct{}
+
+func (c *ConsoleLogger) Log(msg string) {
+	fmt.Printf("%s %s\n", time.Now().Format(time.DateTime), msg)
+}
+
+type IService interface {
+	Do()
+}
+
+type Service struct {
+	logger Logger
+}
+
+func (s *Service) Do() {
+	s.logger.Log("Server is running")
+}
+
+func (s *Service) Log(msg string) {
+	s.logger.Log("[Service]" + msg)
+}
+
+func NewService(c *Container[Logger]) Logger {
+	logger, err := c.Get("logger")
+	if err != nil {
+		return nil
+	}
+
+	return &Service{logger: logger}
 }
--- generic/train_test.go
+++ generic/train_test.go
@@ -1,7 +1,9 @@
 package generic
 
 import (
+	"bytes"
 	"github.com/go-playground/assert/v2"
+	"strconv"
 	"testing"
 )
 
@@ -55,4 +57,118 @@ func TestTrain(t *testing.T) {
 		outputFunc := ApplyDecorator(inputFunc, logger, validator)
 		assert.IsEqual(outputFunc("Alice"), "Hello Alice!")
 	})
+
+	t.Run("sort", func(t *testing.T) {
+		slice := []string{"1", "3", "2", "8", "6", "9", "10"}
+		SortByKey(slice, func(t string) int {
+			i, _ := strconv.Atoi(t)
+			return i
+		}, true)
+		t.Logf("slice: %v\n", slice)
+	})
+
+	t.Run("database", func(t *testing.T) {
+		myString := MyString("Hello world!")
+		dbi := &DBInstance[MyString]{store: make(map[string]MyString)}
+		err := dbi.Put("Print", myString)
+		if err != nil {
+			t.Fatal(err)
+		}
+		data, err := dbi.Get("Print")
+		if err != nil {
+			t.Fatal(err)
+		}
+		body, _ := data.Marshal()
+		t.Logf("data.marshal: %v\n", body)
+		assert.Equal(t, body, []byte("Hello world!"))
+	})
+
+	t.Run("pool", func(t *testing.T) {
+		p1 := Pool[bytes.Buffer]{}
+		p1.Put(*bytes.NewBuffer([]byte("Hello world!")))
+		v := p1.Get()
+		t.Logf("pool get: %s\n", v.Bytes())
+
+		type Foo struct{}
+		p2 := Pool[Foo]{}
+		p2.Put(Foo{})
+		v2 := p2.Get()
+		t.Logf("pool get: %#v\n", v2)
+	})
+
+	t.Run("vector", func(t *testing.T) {
+		vector := &Vector[float64]{}
+		a, b := []float64{1, 2, 3}, []float64{4, 5, 6}
+		assert.IsEqual(vector.Add(a, b), []float64{5, 7, 9})
+		assert.IsEqual(vector.Dot(a, b), 21)
+	})
+
+	t.Run("state-machine", func(t *testing.T) {
+		type state uint
+		const (
+			Freezing state = iota
+			Walking
+			Running
+			Flying
+		)
+
+		type Event = string
+
+		moveEvent := "Move"
+		fastMoveEvent := "Fast move"
+		highSpeedEvent := "High Speed"
+
+		sm := NewStateMachine[state, Event]()
+		sm.AddTransition(Freezing, moveEvent, Walking)
+		sm.AddTransition(Walking, fastMoveEvent, Running)
+		sm.AddTransition(Running, highSpeedEvent, Flying)
+
+		nextState, err := sm.Trigger(moveEvent)
+		if err != nil {
+			t.Fatal(err)
+		}
+		nextState, err = sm.Trigger(fastMoveEvent)
+		if err != nil {
+			t.Fatal(err)
+		}
+		nextState, err = sm.Trigger(highSpeedEvent)
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.IsEqual(nextState, Flying)
+	})
+
+	t.Run("container", func(t *testing.T) {
+		loggerContainer := NewContainer[Logger]()
+
+		logger := &ConsoleLogger{}
+
+		loggerContainer.RegisterInstance(`logger`, logger)
+
+		loggerContainer.RegisterFactory("service", NewService)
+
+		service, err := loggerContainer.Get("service")
+		if err != nil {
+			t.Fatalf("container err: %v\n", err)
+		}
+
+		service.Log("Hello world!")
+
+		// 另一种写法，容器分开写，实际意义不大，增加了冗余容器
+
+		serviceContainer := NewContainer[IService]()
+		serviceContainer.RegisterFactory("service", func(container *Container[IService]) IService {
+			got, err := loggerContainer.Get("logger")
+			if err != nil {
+				return nil
+			}
+			return &Service{logger: got}
+		})
+		service2, err := serviceContainer.Get("service")
+		if err != nil {
+			t.Fatal(err)
+		}
+		service2.Do()
+	})
+
 }
